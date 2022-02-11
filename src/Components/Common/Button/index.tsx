0a40bedcd2816e154de234c4c5b64f8851e01b6e
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-11 16:09:35 +0900

    feat #6: 버튼 컴포넌트 구현

--- src/Components/Common/Button/index.tsx
+++ src/Components/Common/Button/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import 'Components/Common/Button/scss/Button.scss';
 
-const Button = () => {
-  return <></>;
+interface ButtonProps {
+  style: string;
+  text: string;
+  disabled?: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ style, text, disabled, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`button ${style} ${disabled}`}
+      type="button"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
