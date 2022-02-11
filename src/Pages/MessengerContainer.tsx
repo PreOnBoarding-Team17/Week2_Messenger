b77d29a3ef298cd95184cdb53c25bb1c43b9fb19
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 22:06:46 +0900

    feat #11: reply 상태값 추가하고 컴포넌트에 연결

--- src/Pages/MessengerContainer.tsx
+++ src/Pages/MessengerContainer.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MessengerHeader from 'Components/MessengerHeader';
 import MessageList from 'Components/MessageList';
 import MessageInput from 'Components/MessageInput';
 import MessengerLogin from 'Components/MessengerLogin';
 import 'Pages/scss/MessengerContainer.scss';
+import { ReplyDataInterface } from 'Utils/Interface';
 
 const MessengerContainer = () => {
+  const [replyData, setReplyData] = useState<ReplyDataInterface>({
+    id: 0,
+    userName: '',
+    message: '',
+  });
+  // { userId, userName, message }
+
+  // const handlerSetReplayData = (data: ReplyDataInterface) => {
+  //   console.log(123);
+  //   setReplyData(data);
+  // };
+
   return (
     <main className="messenger-container">
       <MessengerHeader />
-      <MessageList />
-      <MessageInput />
+      <MessageList setReplyData={setReplyData} />
+      <MessageInput replyData={replyData} />
     </main>
   );
 };
