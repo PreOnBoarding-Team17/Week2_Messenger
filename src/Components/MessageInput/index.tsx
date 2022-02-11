b970284768188912f7440fc31344fb207178a270
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 23:46:16 +0900

    refactor #5: useTextarea hooks 로 분리

--- src/Components/MessageInput/index.tsx
+++ src/Components/MessageInput/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { replyMessage, sendMessage } from 'Store/Actions';
+import { replyMessage, sendMessage } from 'Store/Actions/message';
 import MessageTextArea from 'Components/MessageInput/MessageTextArea';
 import 'Components/MessageInput/scss/MessageInput.scss';
+import useTextarea from 'Utils/Hooks/useTextarea';
 
 interface MessageInputProps {
   replyData: {
@@ -13,24 +14,21 @@ interface MessageInputProps {
 }
 
 const MessageInput: React.FC<MessageInputProps> = ({ replyData }) => {
-  const [message, setMessage] = useState<string>('');
+  const { message, setMessage, onChangeInput } = useTextarea();
   const [isReply, setIsReply] = useState<boolean>(false);
   const dispatch = useDispatch();
 
-  // userId 받아오는 로직 추가
-
-  // reply 하는 경우 messageId 받아오는 로직 추가
-  // messageId 받아오고 사용자이름, 메세지 내용 받아오기
-
   useEffect(() => {
-    if (replyData.id !== 0) {
-      onClickReply();
+    if (replyData.id !== 0 && !isReply) {
+      setMessage(
+        `${replyData.userName}\n` +
+          `${replyData.message}\n` +
+          `(회신)\n` +
+          message
+      );
+      setIsReply(true);
     }
   }, [replyData]);
-
-  const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
-    setMessage(e.target.value);
-  };
 
   const onClickSend = useCallback((): void => {
     console.log(message);
@@ -42,19 +40,6 @@ const MessageInput: React.FC<MessageInputProps> = ({ replyData }) => {
       setIsReply(false);
     }
   }, [dispatch, message, isReply]);
-
-  const onClickReply = (): void => {
-    if (!isReply) {
-      // setMessage(`사용자 이름\n` + `메시지 내용\n` + `(회신)\n` + message);
-      setMessage(
-        `${replyData.userName}\n` +
-          `${replyData.message}\n` +
-          `(회신)\n` +
-          message
-      );
-    }
-    setIsReply(true);
-  };
 
   const cancelReply = (): void => {
     setIsReply(false);
