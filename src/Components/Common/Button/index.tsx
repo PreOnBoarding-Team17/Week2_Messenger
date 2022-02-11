64160c0cc42e1939929abbace73aaa875bfe39e7
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-11 23:48:18 +0900

    chore: 버튼 disabled 생성

--- src/Components/Common/Button/index.tsx
+++ src/Components/Common/Button/index.tsx
@@ -4,12 +4,18 @@ import 'Components/Common/Button/scss/Button.scss';
 interface ButtonProps {
   style: string;
   text: string;
+  disabled: boolean;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ style, text, onClick }: ButtonProps) => {
+const Button = ({ style, text, onClick, disabled = false }: ButtonProps) => {
   return (
-    <button className={`button ${style}`} type="button" onClick={onClick}>
+    <button
+      className={`button ${style}`}
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
