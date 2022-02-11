100eda2bd9ca73c5a60377d8b31299bba902c4d7
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 17:02:28 +0900

    feat #5: reply 문구 넣어주는 함수 추가, 함수 리턴 타입 설정

--- src/Components/MessageInput/index.tsx
+++ src/Components/MessageInput/index.tsx
@@ -2,20 +2,31 @@ import React, { useState } from 'react';
 import 'Components/MessageInput/scss/MessageInput.scss';
 
 const MessageInput = () => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState<string>('');
+  const [isReply, setIsReply] = useState<boolean>(false);
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setMessage(e.target.value);
   };
 
-  const onClickSend = () => {
+  const onClickSend = (): void => {
     console.log(message);
     setMessage('');
   };
 
+  const onClickReply = (): void => {
+    if (!isReply) {
+      setMessage(`사용자 이름\n` + `메시지 내용\n` + `(회신)\n` + message);
+    }
+    setIsReply(true);
+  };
+
   return (
     <>
-      <section style={{ height: 'calc(80vh - 260px)' }}>임시컨테이너</section>
+      <section style={{ height: 'calc(80vh - 260px)' }}>
+        임시컨테이너
+        <button onClick={onClickReply}>임시reply버튼</button>
+      </section>
       <section className="message-input">
         <div className="message-input__textarea">
           <textarea
