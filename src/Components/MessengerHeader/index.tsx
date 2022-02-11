e2c62a422f90a5f6f23b880c225f4fab7c5bb202
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 15:39:51 +0900

    feat #4: MessengerHeader 컴포넌트 제작

--- src/Components/MessengerHeader/index.tsx
+++ src/Components/MessengerHeader/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import 'Components/MessengerHeader/scss/MessengerHeader.scss';
+import LogoImage from 'Assets/Logo.png';
 
 const MessengerHeader = () => {
   return (
-    <section>
-      <h1>헤더</h1>
+    <section className="messanger-header">
+      <div className="messanger-header__logo">
+        <img src={LogoImage} alt="logo" />
+      </div>
+      <button>로그아웃</button>
     </section>
   );
 };
