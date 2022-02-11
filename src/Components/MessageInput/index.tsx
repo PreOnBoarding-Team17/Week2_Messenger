fa4e10244b5c43f2461d25be1c386b92a346e1f1
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 22:06:10 +0900

    feat #11: replyData 상태 추가

--- src/Components/MessageInput/index.tsx
+++ src/Components/MessageInput/index.tsx
@@ -1,10 +1,18 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { replyMessage, sendMessage } from 'Store/Actions';
 import MessageTextArea from 'Components/MessageInput/MessageTextArea';
 import 'Components/MessageInput/scss/MessageInput.scss';
 
-const MessageInput = () => {
+interface MessageInputProps {
+  replyData: {
+    id: number;
+    userName: string;
+    message: string;
+  };
+}
+
+const MessageInput: React.FC<MessageInputProps> = ({ replyData }) => {
   const [message, setMessage] = useState<string>('');
   const [isReply, setIsReply] = useState<boolean>(false);
   const dispatch = useDispatch();
@@ -14,6 +22,12 @@ const MessageInput = () => {
   // reply 하는 경우 messageId 받아오는 로직 추가
   // messageId 받아오고 사용자이름, 메세지 내용 받아오기
 
+  useEffect(() => {
+    if (replyData.id !== 0) {
+      onClickReply();
+    }
+  }, [replyData]);
+
   const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setMessage(e.target.value);
   };
@@ -22,8 +36,8 @@ const MessageInput = () => {
     console.log(message);
 
     if (message) {
-      if (isReply) dispatch(replyMessage(4, message, 1));
-      else dispatch(sendMessage(4, message));
+      if (isReply) dispatch(replyMessage(3, message, replyData.id));
+      else dispatch(sendMessage(3, message));
       setMessage('');
       setIsReply(false);
     }
@@ -31,7 +45,13 @@ const MessageInput = () => {
 
   const onClickReply = (): void => {
     if (!isReply) {
-      setMessage(`사용자 이름\n` + `메시지 내용\n` + `(회신)\n` + message);
+      // setMessage(`사용자 이름\n` + `메시지 내용\n` + `(회신)\n` + message);
+      setMessage(
+        `${replyData.userName}\n` +
+          `${replyData.message}\n` +
+          `(회신)\n` +
+          message
+      );
     }
     setIsReply(true);
   };
@@ -42,12 +62,11 @@ const MessageInput = () => {
 
   return (
     <>
-      <section>
-        임시컨테이너
-        <button onClick={onClickReply}>임시reply버튼</button>
-      </section>
       <section className="message-input">
         <div className="message-input__left-menu">
+          <div className="message-input__left-menu__textarea">
+            <MessageTextArea value={message} onChange={onChangeInput} />
+          </div>
           {isReply && (
             <button
               className="message-input__left-menu__cancel-btn"
@@ -56,9 +75,6 @@ const MessageInput = () => {
               reply 취소
             </button>
           )}
-          <div className="message-input__left-menu__textarea">
-            <MessageTextArea value={message} onChange={onChangeInput} />
-          </div>
         </div>
         <button
           type="button"
