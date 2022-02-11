9aa38f8f10d1847f1ed430e8d2babeba39866576
som-syom <dhyeon@student.42seoul.kr>
2022-02-11 16:44:57 +0900

    feat #5: MessageInput 컨테이너 템플릿 작성

--- src/Components/MessageInput/index.tsx
+++ src/Components/MessageInput/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'Components/MessageInput/scss/MessageInput.scss';
 
 const MessageInput = () => {
+  const [message, setMessage] = useState('');
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const onClickSend = () => {
+    console.log(message);
+    setMessage('');
+  };
+
   return (
-    <section>
-      <h1>인풋</h1>
-    </section>
+    <>
+      <section style={{ height: 'calc(80vh - 260px)' }}>임시컨테이너</section>
+      <section className="message-input">
+        <div className="message-input__textarea">
+          <textarea
+            name="message"
+            id="message"
+            placeholder="메세지를 입력해주세요 ✍🏻"
+            onChange={onChangeInput}
+            value={message}
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="message-input__send-btn"
+          onClick={onClickSend}
+        >
+          보내기
+        </button>
+      </section>
+    </>
   );
 };
 
